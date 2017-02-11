7fb736fa1b2db46f72b5db1bd8c1df99cb4fb082
juavabar <juavabar@gmail.com>
2017-02-11 20:45:32 +0100

    Añado errores forzados para pruebas

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 var http = require('http')
+var path = require('path')
+var url = require('url')
 
-port = (process.env.PORT || 8080)
+var port = (process.env.PORT || 8080)
 
 function onRequest(req, res) {
-  console.log("OnRequest: " + (new Date()).toString())
+  var pathname = url.parse(req.url).pathname
+  console.log("OnRequest: " + pathname + " " + (new Date()).toString())
   res.writeHead(200, {"Content-type":"text/plain"})
-  res.end('Hello Radiola at ' + (new Date()).toString())
+  var text = 'Hello Radiola-case1 at ' + (new Date()).toString()
+  if (pathname === '/case2') {
+    text = 'Hello Radiola-case2 at ' + (new Date()).toString()
+  }
+  else if (pathname === '/case3') {
+    throw new Error('ForcedException')
+  }
+  res.end(text)
 }
 
 var server = http.createServer(onRequest)
